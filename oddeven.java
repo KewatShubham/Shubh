1d77e47586ddc48c80ed7c27c2b7752a06753d7f
shubham kewat <kewatshubham1999@gmail.com>
2023-07-19 11:34:22 +0530

    First Commit

---
+++ oddeven.java
@@ -0,0 +1,12 @@
+public class evenodd {
+
+    public static void main(String[] args) {
+        int number = 21;
+
+        // checking whether the number is even or odd
+        if (number % 2 == 0)
+            System.out.println(number + " is Even");
+        else
+            System.out.println(number + " is odd");
+    }
+}
